7e939772872caf4595e5f413d63eda66caee0096
Gregory C <sgchris@gmail.com>
2018-02-09 15:10:24 +0200

    added metadata and webapi services. changed DB scheme - added "users"

---
+++ src/app/metadata.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+
+@Injectable()
+export class MetadataService {
+
+    constructor() { }
+
+    set(key:string, value:any) {
+
+    }
+}
---
+++ src/app/webapi.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+
+@Injectable()
+export class WebapiService {
+
+  constructor() { }
+
+  webApi(method, url, params) {
+      
+  }
+
+}
