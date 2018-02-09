800ebc07740a0a617df5a3e91b779942bafcf75b
Gregory C <sgchris@gmail.com>
2018-02-09 22:14:16 +0200

    implemented web-api service

--- src/app/webapi.service.ts
+++ src/app/webapi.service.ts
@@ -1,13 +1,55 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { ConfigService } from './config.service';
 
 @Injectable()
 export class WebapiService {
 
-  constructor() { }
+    constructor(
+        private http: HttpClient,
+        private configService: ConfigService
+    ) { }
 
-  webApi(method, url, params) {
-      
-  }
+    // GET requests
+    get(url, params, callbackFn) {
+        // prepare the URL
+        const requestUrl = this.configService.API_BASE_URL + url + '.php';
 
+        // make the request
+        let promise = this.http.get(requestUrl, params).toPromise();
+
+        // check callback
+        if (callbackFn) {
+            promise.then(callbackFn);
+        }
+
+        return promise;
+    }
+
+    // POST requests
+    post(url, params, callbackFn) {
+        // prepare the URL
+        const requestUrl = this.configService.API_BASE_URL + url + '.php';
+
+        // prepare request parameters
+        let requestParams = new HttpParams();
+        for (let k in params) {
+            requestParams.set(k, params[k]);
+        }
+
+        // set request as form submit
+        const requestOptions = {
+            headers: new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded'),
+        };
+
+        // perform the request
+        let promise = this.http.post(requestUrl, requestParams.toString(), requestOptions).toPromise();
+
+        // check callback
+        if (callbackFn) {
+            promise.then(callbackFn);
+        }
+
+        return promise;
+    }
 }
