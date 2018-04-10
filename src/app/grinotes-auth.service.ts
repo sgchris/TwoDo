1a7adc1f10deb83dc0b07b3286f476b324e1aa6c
Gregory C <sgchris@gmail.com>
2018-04-10 22:16:29 +0300

    added AWS authentication

--- src/app/grinotes-auth.service.ts
+++ src/app/grinotes-auth.service.ts
@@ -2,11 +2,14 @@ import { Injectable, EventEmitter } from '@angular/core';
 import { AuthService } from "angular5-social-login";
 import { FacebookLoginProvider } from "angular5-social-login";
 
+declare var AWS;
+
 @Injectable()
 export class GrinotesAuthService {
     user;
     accessToken;
     isLoggedIn = false;
+    isAWSLoggedIn = false;
 
     // event is emitted upon current file change
     authUpdateEvent: EventEmitter<any> = new EventEmitter();
@@ -14,8 +17,39 @@ export class GrinotesAuthService {
     constructor(private fbAuthService: AuthService) {
         this.fbAuthService.authState.subscribe((user) => {
             this.user = user;
-            this.isLoggedIn = (user != null);
+            this.isLoggedIn = !!this.user;
             this.accessToken = user ? user.token : false;
+
+            if (this.user && !this.isLoggedIn) {
+                AWS.config.region = 'eu-central-1';
+
+                // Add the Facebook access token to the Cognito credentials login map.
+                AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+                    IdentityPoolId: 'eu-central-1:904ba100-069f-4f70-8799-f02a4da81cab',
+                    Logins: {'graph.facebook.com': this.accessToken}
+                });
+
+                this.isAWSLoggedIn = true;
+                /*
+                // Obtain AWS credentials
+                let grinotesAuth = this;
+                AWS.config.credentials.get(_ => {
+                    grinotesAuth.isAWSLoggedIn = true;
+                    // Access AWS resources here.
+                    let params = {
+                        FunctionName: "grinotes_get_files",
+                        Payload: JSON.stringify({
+                            fbid: grinotesAuth.user.id
+                        })
+                    };
+                    let lambda = new AWS.Lambda();
+                    lambda.invoke(params, (err, data) => {
+                        console.log('err', err, 'data', data);
+                    });
+
+                });
+                */
+            }
 
             // fire event forward
             this.authUpdateEvent.emit(this.user);
