06facc571112a3935884a56f8b6ef720002787fb
Gregory Chris <sgchris@gmail.com>
2018-05-17 21:58:23 +0300

    deleted old griCKEditor files

--- src/app/grickeditor.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { GrickeditorDirective } from './grickeditor.directive';
-
-describe('GrickeditorDirective', () => {
-  it('should create an instance', () => {
-    const directive = new GrickeditorDirective();
-    expect(directive).toBeTruthy();
-  });
-});
